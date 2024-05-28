d9deac9f8205cecda7617881b3b94f8ec7e9a538
Lucas Silva <lucasbarboza299@gmail.com>
2024-05-28 16:43:39 -0300

    corrigido bug na tela cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -2,80 +2,32 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
 import * as Animatable from "react-native-animatable";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useNavigation } from '@react-navigation/native';
+import { createUserWithEmailAndPassword, fetchSignInMethodsForEmail} from "firebase/auth";
 import { auth, firestore } from "../../services/firebaseConnection"; 
 import { doc, setDoc } from "firebase/firestore"; 
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function Cadastro() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [fullName, setFullName] = useState('');
-  const [birthDate, setBirthDate] = useState('');
-  const [fieldsFilled, setFieldsFilled] = useState(false); // Novo estado para controlar se os campos estão preenchidos
   const navigation = useNavigation();
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    fullName: '',
+    birthDate: '',
+  });
 
-  // Função para verificar se todos os campos obrigatórios estão preenchidos
-  const checkFields = () => {
-    if (email && password && confirmPassword && fullName && birthDate) {
-      setFieldsFilled(true);
-    } else {
-      setFieldsFilled(false);
-      const emptyFields = [];
-      if (!email) emptyFields.push('Email');
-      if (!password) emptyFields.push('Senha');
-      if (!confirmPassword) emptyFields.push('Confirmação de Senha');
-      if (!fullName) emptyFields.push('Nome Completo');
-      if (!birthDate) emptyFields.push('Data de Nascimento');
-      Alert.alert('Erro', `Por favor, preencha todos os campos obrigatórios:\n${emptyFields.join(', ')}`);
-
-    }
-  };
-
-  async function checkEmailExists(email) {
-    try {
-      const methods = await fetchSignInMethodsForEmail(auth, email);
-      if (methods && methods.length > 0) {
-        // E-mail já foi usado
-        return true;
-      } else {
-        // E-mail ainda não foi usado
-        return false;
-      }
-    } catch (error) {
-      console.error("Erro ao verificar e-mail: ", error);
-      return true; // Considerando um erro como um e-mail já existente para evitar falsos negativos
-    }
-  }
-
-  const formatBirthDate = (value) => {
-    const cleaned = ('' + value).replace(/\D/g, '');
+  const createUser = async () => {
+    const { email, password, confirmPassword, fullName, birthDate } = userData;
   
-    let formatted = cleaned;
-    if (cleaned.length > 2) {
-      formatted = cleaned.slice(0, 2) + '/' + cleaned.slice(2);
-    }
-    if (cleaned.length > 4) {
-      formatted = formatted.slice(0, 5) + '/' + cleaned.slice(4);
-    }
-    return formatted;
-  };
-  
-  async function createUser() {
-    if (!fieldsFilled) {
-      checkFields(); // Verifica se todos os campos estão preenchidos antes de tentar cadastrar
+    if (!email || !password || !confirmPassword || !fullName || !birthDate) {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos obrigatórios.');
       return;
     }
   
     if (password !== confirmPassword) {
       Alert.alert('Erro', 'As senhas não coincidem!');
-      return;
-    }
-  
-    const emailExists = await checkEmailExists(email);
-    if (emailExists) {
-      Alert.alert('Erro', 'Este e-mail já está sendo usado por outra conta.');
       return;
     }
   
@@ -85,19 +37,42 @@ export default function Cadastro() {
   
       await setDoc(doc(firestore, 'users', user.uid), {
         uid: user.uid,
-        email: email,
-        fullName: fullName,
-        birthDate: birthDate,
+        email,
+        fullName,
+        birthDate,
       });
   
       console.log('Cadastrado com sucesso! \n ' + user.uid);
       Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!');
-      navigation.navigate('Login');
+      navigation.navigate('Login'); 
     } catch (error) {
-      console.error("Erro ao cadastrar usuário: ", error);
-      Alert.alert('Erro', error.message);
+      if (error.code === 'auth/email-already-in-use') {
+        Alert.alert('Erro', 'Este e-mail já está sendo usado por outra conta.');
+      } else {
+        console.error("Erro ao cadastrar usuário: ", error);
+        Alert.alert('Erro', error.message);
+      }
     }
-  }
+  };
+  
+
+  const checkEmailExists = async (email) => {
+    try {
+      const methods = await fetchSignInMethodsForEmail(auth, email);
+      return methods && methods.length > 0;
+    } catch (error) {
+      console.error("Erro ao verificar e-mail: ", error);
+      return true;
+    }
+  };
+
+  const formatBirthDate = (value) => {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '$1/$2')
+      .replace(/(\d{2})(\d)/, '$1/$2')
+      .replace(/(\d{4})(\d+)$/, '$1');
+  };
   
   return (
     <View style={estilos.container}>
@@ -111,16 +86,16 @@ export default function Cadastro() {
           <TextInput 
             placeholder='Digite seu nome completo' 
             style={estilos.input}
-            value={fullName}
-            onChangeText={value => setFullName(value)}
+            value={userData.fullName}
+            onChangeText={(value) => setUserData({ ...userData, fullName: value })}
           />
 
           <Text style={estilos.title}>Email</Text>
           <TextInput 
             placeholder='Digite um email' 
             style={estilos.input} 
-            value={email}
-            onChangeText={value => setEmail(value)}
+            value={userData.email}
+            onChangeText={(value) => setUserData({ ...userData, email: value })}
           />
 
           <Text style={estilos.title}>Senha</Text>
@@ -128,8 +103,8 @@ export default function Cadastro() {
             placeholder='Digite a senha' 
             style={estilos.input} 
             secureTextEntry 
-            value={password}
-            onChangeText={value => setPassword(value)} 
+            value={userData.password}
+            onChangeText={(value) => setUserData({ ...userData, password: value })}
           />
 
           <Text style={estilos.title}>Confirme a Senha</Text>
@@ -137,8 +112,8 @@ export default function Cadastro() {
             placeholder='Confirme sua senha' 
             style={estilos.input} 
             secureTextEntry 
-            value={confirmPassword}
-            onChangeText={value => setConfirmPassword(value)} 
+            value={userData.confirmPassword}
+            onChangeText={(value) => setUserData({ ...userData, confirmPassword: value })}
           />
 
           <Text style={estilos.title}>Data de Nascimento</Text>
@@ -146,8 +121,8 @@ export default function Cadastro() {
             placeholder='DD/MM/AAAA' 
             style={estilos.input}
             keyboardType='numeric'
-            value={birthDate}
-            onChangeText={value => setBirthDate(formatBirthDate(value))}
+            value={userData.birthDate}
+            onChangeText={(value) => setUserData({ ...userData, birthDate: formatBirthDate(value) })}
             maxLength={10} 
           />
 
@@ -216,4 +191,4 @@ const estilos = StyleSheet.create({
   },
 });
 
-   
+
